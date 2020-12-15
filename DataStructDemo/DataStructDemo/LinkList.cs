3a2ed716a42df2dd13829e79f7d35958f25fd113
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-15 17:03:58 +0530

    [Pratibha Mastud] Add: Create LinkedList Data Structure

---
+++ DataStructDemo/DataStructDemo/LinkList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace DataStructDemo
+{
+    class LinkList
+    {
+        internal Node head;
+
+        internal void insertData(int data)
+        {
+            Node newnode = new Node(data);
+            if (head == null)
+            {
+                head = newnode;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = newnode;
+            }
+            Console.WriteLine("{0} - Added in linklist",newnode.data);
+        }
+
+        internal void Display() 
+        {
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is Empty");
+                return;
+            }
+
+            while (temp != null) 
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
